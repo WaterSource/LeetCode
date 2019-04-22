60974bbc8d77a4ad5d2df7233e5a01e0dffca829
何源 <heyuan@imdada.cn>
2019-04-22 21:15:07 +0800

    [ADD] 两数之和

---
+++ Swift/Array/两数之和.swift
@@ -0,0 +1,21 @@
+/**
+ * Question Link: https://leetcode.com/problems/two-sum/
+ * Primary idea: Traverse the array and store target - nums[i] in a dict
+ *
+ * Time Complexity: O(n), Space Complexity: O(n)
+ */
+
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        var dict = [Int: Int]()
+        
+        for (i, num) in nums.enumerated() {
+            if let index = dict[target - num] {
+                return [index, i]
+            }
+            dict[num] = i
+        }
+        
+        fatalError("No valid outputs")
+    }
+}
